372123f851fd8ce8e3829dadc86880b16b4a07af
bquizza5 <blaine555@gmail.com>
2020-03-16 14:24:15 -0600

    added home page

---
+++ front-end/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = (props) => {
+
+
+    return (
+        <div>
+            
+            this is the home page
+        </div>
+    )
+}
+export default Home;
