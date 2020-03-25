431ec564aa7d5c230137e1ae72df1956518f2195
bquizza5 <blaine555@gmail.com>
2020-03-25 15:08:25 -0600

    added labels to some inputs

--- front-end/src/components/home.js
+++ front-end/src/components/home.js
@@ -6,9 +6,10 @@ const Home = (props) => {
 
     return (
         <div>
+            <p>this app has 2 tools. the first one is a future value calculator and the second one is a retirement planner.</p>
             <button onClick={() => { props.setTab('calc') }}>calculator</button>
             <button onClick={() => { props.setTab('plan') }}>planner</button>
-            <p>this is the home page</p>
+            
         </div>
     )
 }
