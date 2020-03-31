ef9ad16f356e0393314424f8ff4daa0e01d00540
bquizza5 <blaine555@gmail.com>
2020-03-31 17:07:35 -0600

    slides working

--- front-end/src/components/slideShow.js
+++ front-end/src/components/slideShow.js
@@ -6,12 +6,13 @@ import { updateYwr, updateInflation, updateAllowance, updateN, updateMagicNum, u
 
 
 
-const SlideShow = () => {
+const SlideShow = (props) => {
     const [currentSlide, setCurrentSlide] = useState(1)
     const data = [
-        { question: "If you were to retire today, how much money would you need per month?", setter: (e) => { console.log(e) }, slideNumber: 1 },
-        { question: "What percent of your retirement balance will you withdraw yearly? (this should be less than your average expected return or you will run out of money)", setter: (e) => { console.log(e) }, slideNumber: 2 },
-        { question: "In how many years do you plan to retire?", setter: (e) => { console.log(e) }, slideNumber: 3 },
+        { question: "If you were to retire today, how much money would you need per month?", setter: (e) => { props.updateAllowance(e) }, slideNumber: 1 },
+        { question: "What percent of your retirement balance will you withdraw yearly? (this should be less than your average expected return or you will run out of money)", setter: (e) => { props.updateYwr(e) }, slideNumber: 2 },
+        { question: "In how many years do you plan to retire?", setter: (e) => { props.updateN(e) }, slideNumber: 3 },
+        { question: "What will the average inflation rate be? (historically is been about 3%)", setter: (e) => { props.updateInflation(e) }, slideNumber: 4 },
     ]
 
     const nextSlide = () => {
@@ -38,20 +39,20 @@ const SlideShow = () => {
     )
 }
 
-// const mapPropsToState = state => {
-//     return { state: state };
-// };
+const mapPropsToState = state => {
+    return { state: state };
+};
 
-// export default connect(
-//     mapPropsToState,
-//     {
-//         updateYwr,
-//         updateInflation,
-//         updateAllowance,
-//         updateN,
-//         updateMagicNum,
-//         updatePV,
-//         updateI
-//     })(SlideShow);
+export default connect(
+    mapPropsToState,
+    {
+        updateYwr,
+        updateInflation,
+        updateAllowance,
+        updateN,
+        updateMagicNum,
+        updatePV,
+        updateI
+    })(SlideShow);
 
-export default SlideShow;
+// export default SlideShow;
