ac84652ebfa046b2bc838a608d1d9792e1761cbb
bquizza5 <blaine555@gmail.com>
2020-05-21 14:33:50 -0600

    button fixed

--- front-end/src/components/home.js
+++ front-end/src/components/home.js
@@ -5,7 +5,8 @@ const Home = (props) => {
 
 
     return (
-        <div>
+        <div className='home'>
+            <h1>Blaine's Calculator</h1>
             <p>this app has 2 tools. the first one is a future value calculator and the second one is a retirement planner.</p>
             <button onClick={() => { props.setTab('calc') }}>calculator</button>
             <button onClick={() => { props.setTab('plan') }}>planner</button>
